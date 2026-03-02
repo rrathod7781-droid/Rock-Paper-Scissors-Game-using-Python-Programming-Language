ad7a6892ddc9f7bb2d9e90321952f3ae360b0c55
Rohit Rathod <rrathod7781@gmail.com>
2026-03-02 17:11:11 +0530

    Add files via upload

---
+++ Rock_Paper_Scissors_Game.py
@@ -0,0 +1,42 @@
+#Building my first game in python by my self 
+import random as r
+
+def game_main():
+    list1 = ["rock", "paper", "scissors"]
+    while True:
+        user_choice = input("Enter your choice (rock, paper, scissors): ").lower()
+        computer_choice = r.choice(list1)
+        print(f"Computer's choice is : {computer_choice}")
+        if (user_choice == computer_choice):
+            print("It's a tie!")
+            return 0
+        elif (user_choice == "rock" and computer_choice == "scissors") or (user_choice == "paper" and computer_choice == "rock") or (user_choice == "scissors" and computer_choice == "paper"):
+            print("Congratulations! You win!😊💕")
+            return 1
+        elif (user_choice == "rock" and computer_choice == "paper") or (user_choice == "paper" and computer_choice == "scissors") or (user_choice == "scissors" and computer_choice == "rock"):
+            print("It Sad 😭 You lose!")
+            return -1
+        else:
+            print("Invalid input! Please enter rock, paper, or scissors.")
+            return 0             
+
+user_choice = 0
+computer_choice = 0
+
+n = int(input("Enter How many rounds you want to play : "))
+
+for i in range(1,n+1):
+    print(f"Round {i} :")
+    result = game_main()
+
+    if (result == 1):
+        user_choice += 1
+    elif (result == -1):
+        computer_choice += 1
+
+if (user_choice == computer_choice):
+    print(f"\nIt's a tie overall!👍 \nScore of Computer is : {computer_choice} \nYour Score is : {user_choice}")
+elif (user_choice > computer_choice):
+    print(f"\nCongratulations! You win overall!😊💕\nScore of Computer is : {computer_choice} \nYour Score is : {user_choice}")
+else:
+    print(f"\nComputer wins overall! 😭\nScore of Computer is : {computer_choice} \nYour Score is : {user_choice}")
